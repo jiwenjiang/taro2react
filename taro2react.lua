2847b81f74d91900b73b891b1b5c11a50f98abc9
jiwenjiang <513755565@qq.com>
2024-12-23 10:48:25 +0800

    feat: update rex

--- taro2react.lua
+++ taro2react.lua
@@ -11,20 +11,34 @@ function OpenFile(path, ext)
     local content = file:read("*a")
     file:close()
     if ext ~= "tsx" then
-        content = rex.gsub(content, "(\\d+px)", function(m)
+        content = rex.gsub(content, "(\\d+(px|rpx))", function(m)
             local num = m:match("%d+")
             return math.ceil(num / 2) .. "px"
         end)
+        content = rex.gsub(content, "(\\d+(PX))", function(m)
+            local num = m:match("%d+")
+            return math.ceil(num) .. "px"
+        end)
     else
+        if string.find(content, 'useRouter') then
+            content = rex.gsub(content, "const router = useRouter", "const navigate = useNavigate")
+            content = 'import { useNavigate } from "react-router-dom";' .. "\n" .. content
+            content = content:gsub("navigateTo%({[^}]-%s*url: [`\"]([^`\"]-)[`\"][^}]-%}%)", "navigate(`%1`)")
+            content = content:gsub("router.params.", "")
+        end
         content = rex.gsub(content, "(<View|<ScrollView)(?!\\w)", "<div")
         content = rex.gsub(content, '</View>|</ScrollView>', '</div>')
         content = rex.gsub(content, "<Text(?!\\w)", "<span")
         content = content:gsub("</Text>", "</span>")
         content = rex.gsub(content, "<Image(?!\\w)", "<img")
         content = content:gsub("</Image>", "</img>")
+        content = content:gsub(".scss", ".less")
+        content = content:gsub("Notify%.open%({[^}]-color:%s*\"warning\"[^}]-message:%s*\"(.-)\"%}",
+            "Notify.show({ type: \"warning\", message: \"%1\" })")
     end
 
-    local outputPath = "react_" .. path
+    local newPath = rex.gsub(path, ".scss", ".less")
+    local outputPath = "react_" .. newPath
 
     file, err = io.open(outputPath, "w")
     if not file then
